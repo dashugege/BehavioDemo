de3a13c8135bd8b249bfd26bd203433ab8de7ffe
zongkelong <zongkelong@gmail.com>
2019-04-01 19:24:55 +0800

    app 状态管理变更为redux

--- lib/app_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:scoped_model/scoped_model.dart';
-
-class AppModel extends Model {
-  static AppModel of(BuildContext context) {
-    return ScopedModel.of<AppModel>(context);
-  }
-}
